f9f28baa17f1d4e4d4873eecc0d0d2a1e33d15c7
Cecile <cecile8720@gmail.com>
2018-12-06 00:11:43 -0800

    started on grocery list

--- js/grocerylist.js
+++ js/grocerylist.js
@@ -1,26 +1,38 @@
 $(document).ready(function () {
 
-	$('#list-items').html(localStorage.getItem('listItems'));
+	$('#list-items2').html(localStorage.getItem('listItems'));
+	$('#list-items').html(localStorage.getItem('groceries'));
+
 	$('.add-items').submit(function(event)
 	{
 		event.preventDefault();
 		var item = $('#input').val();
-		if(item)
-		{
+		if(item) {
+			/*
 			$('#list-items').append("<li><input class='checkbox' type='checkbox'/>" + item + "<a class='remove'>x</a><hr></li>");
 			localStorage.setItem('listItems', $('#list-items').html());
+			$('#input').val("");*/
+
+			$('#list-items').append("<li><input class='checkbox' type='checkbox'/>" + item + "<a class='remove'>X</a><hr></li>");
+			localStorage.setItem('groceries', $('#list-items').html());
 			$('#input').val("");
 		}
-
 	});
 
 	$(document).on('change', '.checkbox', function() {
+		// when item gets unchecked
 		if($(this).attr('checked')) {
 			$(this).removeAttr('checked');
 		}
 
+		// when item gets checked
 		else {
 			$(this).attr('checked', 'checked');
+			// send item to ingredients list
+			var word = this.closest('li').innerText;
+			word = word.slice(0,-2);
+			$('#list-items2').append("<li><input class='checkbox' type='checkbox'/>" + word + "<a class='remove'>X</a><hr></li>");
+			localStorage.setItem('listItems', $('#list-items2').html());
 		}
 		$(this).parent().toggleClass('completed');
 		localStorage.setItem('listItems', $('#list-items').html());
@@ -28,7 +40,7 @@ $(document).ready(function () {
 
 	$(document).on('click', '.remove', function() {
 		$(this).parent().remove();
-		localStorage.setItem('listItems', $('#list-items').html());
+		localStorage.setItem('listItems', $('#groceries').html());
 	});
 
 });
